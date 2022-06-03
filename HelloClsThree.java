4a3d1d89c5bf75f3996d9cf8b328c63790a4cbc7
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-06-03 23:00:59 +0530

    Add files via upload

---
+++ HelloClsThree.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package accessSpecifier;
+
+/**
+ *
+ * @author Saubali
+ */
+public class HelloClsThree extends HelloClsTwo{
+    public String greet4="hello world from class three";
+    protected String greet5="hello world from class three protected";
+}
