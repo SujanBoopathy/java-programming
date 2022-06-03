e37886dc11ad0aeb183348c3f6516e82fddf6d46
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-06-03 23:01:16 +0530

    Add files via upload

---
+++ HelloClsTwo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package accessSpecifier;
+
+/**
+ *
+ * @author Saubali
+ */
+public class HelloClsTwo {
+    private String greet1="private hello";
+    public String greet2="public hello ";
+    protected String greet3="protected hello";
+    public String greeting(){
+        return greet1;
+    }
+}
