67d8ef0d2b0e5352598a2af482e264d86d1950d4
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-06-03 22:59:01 +0530

    Add files via upload

---
+++ ClassTwo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clsObj;
+
+/**
+ *
+ * @author Saubali
+ */
+public class ClassTwo {
+    public int i=9;
+    public void greeting(){
+        System.out.println("Hello world");
+    }
+}
